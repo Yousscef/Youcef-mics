96951ccb8cf835d9c9a576693b0f6961e3c94bae
yousscef <youcef.hocine@outlook.fr>
2022-08-31 17:27:50 +0200

    Looks good ! juste one promblem

--- guest.js
+++ guest.js
@@ -1,102 +1,63 @@
-//Les coffres
-var chests = []; 
+//Le nombre de coffres
+var chestsNbr = 0;
+//le nombre de coffres non vides 
+var chestsNbrNotEmpty = 0;
+//Les faux cofres ;)
+var FalseChestsNbr = 0;
+//Les coffres qui ne se sont pas ouvert correctement
 var ErrChest = 0;
+//L'accès aux pièces qui ne s'est pas déroulé correctement
 var ErrRoom = 0;
+
 //Récupérer le contenu de tous les cofres d'une pièce
 async function fetchAllChestReference(chestReference) {
-const results = await Promise.all(chestReference.map((url) => fetch('https://infinite-castles.herokuapp.com' + url).then(r => r.json()).catch(error=>{ ErrChest++; })));
- console.log(results);
- chests.concat(results);
+
+const results = await Promise.all(chestReference.map((url) => fetch('https://infinite-castles.herokuapp.com' + url)
+	.then(r => r.json())
+	.catch(error=>{ ErrChest++; })));
+	for (var i = 0; i < results.length; i++) 
+	{
+		if (results[i] === undefined)
+		{
+			FalseChestsNbr++;
+		} else if (results[i].status === 'It looks like there is something here!')
+		{
+				chestsNbrNotEmpty++;
+		}
+		chestsNbr++;
+	}
 }
 //Rentrer dans toutes les pièces du chateau
 async function fetchAllRooms(rooms) {
 	await Promise.all(rooms.map(room => fetch(room)
-	.then(res => { if (res.ok ) {
-							res.json().then(data => {
-																JsonRespense = data; 
-																console.log(JsonRespense);
-																if (JsonRespense.chests != null){
-																	fetchAllChestReference(JsonRespense.chests)
-																}
-																if (JsonRespense.rooms != null){
-																	fetchAllRooms((JsonRespense.rooms).map(room => 'https://infinite-castles.herokuapp.com' + room));
-																}
-															}
-												)
-						}else  {console.log("Erreur");}	
+	.then(res => { 
+		            if (res.ok ) 
+						             {  
+						             	res.json().then(data => 
+						               	{
+													JsonRespense = data; 
+													if (JsonRespense.chests != null)
+													{
+														fetchAllChestReference(JsonRespense.chests);
+													}
+													if (JsonRespense.rooms != null)
+													{
+														fetchAllRooms((JsonRespense.rooms).map(room => 'https://infinite-castles.herokuapp.com' + room));
+													}
+													
+												})
+												
+						} else  {ErrRoom++;}
+					 }
 
-					}
-			).catch(error=>{ ErrRoom++; })
+			).catch(error=>{ ErrRoom++})
 		)
 	)
+	console.log('Nombre de cofres non vides: ' + chestsNbrNotEmpty);
+	console.log('Nombre de cofres : ' + chestsNbr);
+	console.log('Erreur ouverture de coffre : ' + ErrChest);
+	console.log('Erreur entrée pièce : ' + ErrRoom);
 }
 
-//Main
-fetchAllRooms(['https://infinite-castles.herokuapp.com/castles/1/rooms/entry']);
-for (var i = chests.length - 1; i >= 0; i--) {
-	console.log(chests[i]);
-}
+fetchAllRooms(['https://infinite-castles.herokuapp.com/castles/1/rooms/entry'])
 
-//------------------------------------------------------------------------
-//Version 1.0
-//------------------------------------------------------------------------
-/*var AllChest = null
-
-function seeing_arround(reference){
-
-	var requestURL = 'https://infinite-castles.herokuapp.com' + reference;
-	
-	var XMLHttpRequest = require('xhr2');
-	var request = new XMLHttpRequest();
-	
-	
-
-	request.open('GET', requestURL);
-	request.responseType = 'json';
-	request.send();
-	request.onload = function() {
-		var objects = request.response;
-		if (objects.chests != null) {
-			console.log(' ------ tous les coffres ------', objects.chests)
-			count_chests(objects.chests, nbChests, NbNotEmptyChests);
-		}
-		if (objects.rooms != null) {
-			for (var i = 0; i < objects.rooms; i++) {
-				seeing_arround(objects.rooms[i], nbChests, NbNotEmptyChests);
-			}
-		}
-	}
-}
-
-async function count_chests(Mychests, nbChests, NbNotEmptyChests){
-		
-		 
-  	const results = await Promise.all(Mychests.map((url) => fetch(url).then((r) => r.json())));
- 	console.log(JSON.stringify(results, null, 2));
-		
-		
-		/*var chts = req.response;
-		console.log('coffre : response ' + req.response)
-		if (chts.status != 'This chest is empty :/ Try another one!') {
-				NbNotEmptyChests = NbNotEmptyChests + 1
-			} 
-	 	}
-}
-
- 
-
-var fin, début;
-
-var nbChests = 0, NbNotEmptyChests = 0;
-
-start = new Date();
-
-seeing_arround('/castles/1/rooms/entry', nbChests, NbNotEmptyChests);
-
-end = new Date();
-
-console.log('Durée du parcours : ' + (end.getTime() - start.getTime()) + ' msec');
-console.log('Nombre de cofres : ' + nbChests);
-console.log('Nombre de cofres non vide : ' + NbNotEmptyChests);
-
-*/
