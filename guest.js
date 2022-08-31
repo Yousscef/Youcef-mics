0677c1c6affca007c881cc147b550b18a7f878de
yousscef <youcef.hocine@outlook.fr>
2022-08-31 13:30:44 +0200

    Second approch is good. But still have some bugs to fix

--- guest.js
+++ guest.js
@@ -1,18 +1,31 @@
-var chests = [	"/castles/1/chests/03465e67-f5ba-4696-ba19-dc6f8e48db0d",
-				"/castles/1/chests/544c5ade-fd44-453d-b94c-c8283bf7e9f0",
-				"/castles/1/chests/61690f30-5cc4-464a-b554-bb6bac976995",
-				"/castles/1/chests/2ae4aae4-3b30-4fcf-a808-819ea5a98082",
-				"/castles/1/chests/cb2c7e84-4b39-47d4-bcb2-a8dee0f4c4cd",
-				"/castles/1/chests/968ce288-090f-4f5b-99af-dfbf6389c979",
-				"/castles/1/chests/85871565-c4c6-42bc-95d0-45b4b637c2f2",
-				"/castles/1/chests/5b4f8cc0-d9be-4e24-b0a7-5ad00e0e8f2e",
-				"/castles/1/chests/13dd15b3-2275-4788-8cea-1e8aec534eef"];
-async function fetchAll() {
-const results = await Promise.all(chests.map((url) => fetch('https://infinite-castles.herokuapp.com' + url).then((r) => r.json())));
- console.log(JSON.stringify(results, null, 2));
+//Les coffres
+var chests = []; 
+//Récupérer le contenu de tous les cofres d'une pièce
+async function fetchAllChestReference(chestReference) {
+const results = await Promise.all(chestReference.map((url) => fetch('https://infinite-castles.herokuapp.com' + url).then(r => r.json())));
+ console.log(results);
+ chests.concat(results);
+}
+//Rentrer dans toutes les pièces du chateau
+async function fetchAllRooms(rooms) {
+	const JsonRespense = await Promise.all(rooms.map(room => fetch(room)
+	.then(res => res.json())
+	));
+	console.log(JsonRespense);
+	if (JsonRespense[0].chests != null){
+		fetchAllChestReference(JsonRespense[0].chests)
+	}
+	if (JsonRespense[0].rooms != null){
+		fetchAllRooms((JsonRespense[0].rooms).map(room => 'https://infinite-castles.herokuapp.com' + room));
+	}
 }
 
-fetchAll();
+
+fetchAllRooms(['https://infinite-castles.herokuapp.com/castles/1/rooms/entry']);
+for (var i = chests.length - 1; i >= 0; i--) {
+	console.log(chests[i]);
+}
+
 //------------------------------------------------------------------------
 //Version 1.0
 //------------------------------------------------------------------------
